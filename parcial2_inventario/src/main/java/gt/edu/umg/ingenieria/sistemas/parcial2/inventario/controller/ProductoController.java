cc8245140126e82afc796304d3a5446728377d64
Josue <josuedlavs@gmail.com>
2019-04-24 19:11:07 -0600

    Segundo Parcial

--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
@@ -5,6 +5,10 @@ import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service.ProductoServic
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,16 @@ public class ProductoController {
     public List<ProductoEntity> buscarTodos() {
         return this.productoService.buscarTodos();
     }
+    
+    @PostMapping("/resgistaProducto")
+    public ProductoEntity registraProducto(@RequestBody(required = true) ProductoEntity p1){
+        return this.productoService.registrarProducto(p1);
+    }
+    
+    @PutMapping("/acualizarStock/{id}/{opcion}/{cantidad}")
+    public ProductoEntity actualizarStock(@PathVariable(required = true)long id, @PathVariable(required = true)String opcion, @PathVariable(required = true) int cantidad){
+        return this.productoService.actualizarStock(id, opcion, cantidad);
+    }
+    
     
 }
--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
@@ -13,7 +13,25 @@ public class ProductoService {
     private ProductoRepository productoRepository;
     
     public List<ProductoEntity> buscarTodos() {
-        return (List<ProductoEntity>) this.productoRepository.findAll();
+        return (List<ProductoEntity>) this.productoRepository.buscar();
     }
     
+    public ProductoEntity registrarProducto(ProductoEntity p1){
+        return this.productoRepository.save(p1);
+    }
+    
+    public ProductoEntity actualizarStock(long id, String opcion, int cantidad){
+        ProductoEntity p1 = new ProductoEntity();
+        p1 = this.productoRepository.findById(id).get();
+        p1.setStock(cantidad);
+        
+        if(opcion.toLowerCase().equals("incrementar")){
+            p1.setStock(p1.getStock() + cantidad);
+        }
+        else if(opcion.toLowerCase().equals("decrementar")){
+            p1.setStock(p1.getStock() - cantidad);
+        }
+        
+        return this.productoRepository.save(p1);
+    }
 }
